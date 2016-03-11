d6126157a17d7166ee83d9d612ec60ad41cebc7a
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-03-11 16:58:33 +0100

    :art: Footer

--- js/script.js
+++ js/script.js
@@ -13,7 +13,24 @@ function scrollPage(){
 	requestAnimFrame(scrollPage);
 }
 
+// Fonction pour animer le compteur
+function compteur(){
+	// valeur max compteur
+	var valeurMaxCompteur = 1456622570;
+	// valeur compteur init
+	var valeurCompteur = 1456622470;
+    var compteur = new Odometer({ el: $('.compteur-participants')[0], theme: 'train-station', value: valeurCompteur });
+    compteur.render();
+
+    setInterval(function(){
+    	if (valeurCompteur <= valeurMaxCompteur) {
+    		compteur.update(valeurCompteur++);	
+    	}
+    }, 3000);
+}
+
 $(function(){
+	compteur();
 	$("#etapes-event >li").mouseenter(function() {
 		$("#etapes-event").removeClass("etape-1-active etape-2-active etape-3-active etape-4-active").addClass($(this).attr("class")+"-active");
 	});
